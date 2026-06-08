9165676c7cf263c1af9a0076bed60f12a82708ab
samoka-star <samuelokabore@gmail.com>
2026-06-08 21:50:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Tech Bot",
   botOwner: 'samoka', // Your name
-  ownerNumber: '256743033258', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '256705272534', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
